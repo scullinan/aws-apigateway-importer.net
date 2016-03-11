4f8f93074beb7bcbf2ae4f56d8b608bf454f1288
Prasanna Krishnamoorthy <prasanna.krishnamoorthy@intelliflo.com>
2016-03-11 16:57:36 +0000

    Adding unit tests

---
+++ Importer.Tests/Swagger/Aws/ApiGatewaySdkDeploymentProviderTest.cs
@@ -0,0 +1,45 @@
+using Amazon.APIGateway;
+using Amazon.APIGateway.Model;
+using Importer.Swagger.Aws.Impl;
+using Moq;
+using NUnit.Framework;
+
+namespace Importer.Tests.Swagger.Aws
+{
+    [TestFixture]
+    public class ApiGatewaySdkDeploymentProviderTest
+    {
+        private ApiGatewaySdkDeploymentProvider underTest;
+        private Mock<IAmazonAPIGateway> gatewayMock;
+        private string apiId = "pwfy7quvxh";
+
+        public ApiGatewaySdkDeploymentProviderTest()
+        {
+            gatewayMock = new Mock<IAmazonAPIGateway>();
+            underTest = new ApiGatewaySdkDeploymentProvider(gatewayMock.Object);
+        }
+
+        [Test]
+        public void CreateDeploymentTest()
+        {
+            var config = TestHelper.Import<DeploymentConfig>("deployment.json");
+            
+            gatewayMock.Setup(x => x.PutIntegration(It.IsAny<PutIntegrationRequest>()))
+                .Returns(new PutIntegrationResponse());
+
+            underTest.CreateDeployment(apiId, config);
+
+            gatewayMock.Verify(x => x.CreateDeployment(It.IsAny<CreateDeploymentRequest>()), Times.Once);
+            gatewayMock.Verify(x => x.UpdateStage(It.IsAny<UpdateStageRequest>()), Times.Once);
+        }
+
+        [Test]
+        public void CreateDomainTest()
+        {
+            var config = TestHelper.Import<DeploymentConfig>("deployment.json");
+
+            underTest.CreateDomain(config);
+            gatewayMock.Verify(x => x.CreateDomainName(It.IsAny<CreateDomainNameRequest>()), Times.Once);
+        }
+    }
+}
