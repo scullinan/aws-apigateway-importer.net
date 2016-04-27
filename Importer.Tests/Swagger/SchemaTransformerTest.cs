c75844c81f43f0e0a256d8ce4befa4081b92f6e7
Prasanna Krishnamoorthy <prasanna.krishnamoorthy@intelliflo.com>
2016-04-27 12:26:52 +0100

    Updating SchemaTransformation to fix duplicate datatypes

---
+++ Importer.Tests/Swagger/SchemaTransformerTest.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using Importer.Swagger;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using NUnit.Framework;
+
+namespace Importer.Tests.Swagger
+{
+    [TestFixture]
+    public class SchemaTransformerTest
+    {
+        [Test]
+        public void ComplexModelTest()
+        {
+            var models = Encoding.UTF8.GetString(SchemaResource.defintions);
+            var modelSchema = Encoding.UTF8.GetString(SchemaResource.complex_schema);
+
+            var schemaString = new SchemaTransformer().Flatten(modelSchema, models);
+            var schema = Deserialize(schemaString);
+
+            Assert.That(schema.Properties.Count, Is.EqualTo(16));
+        }
+
+        [Test]
+        public void SimpleModelTest()
+        {
+            var models = Encoding.UTF8.GetString(SchemaResource.defintions);
+            var modelSchema = Encoding.UTF8.GetString(SchemaResource.simple_schema);
+
+            var schemaString = new SchemaTransformer().Flatten(modelSchema, models);
+            var schema = Deserialize(schemaString);
+
+            Assert.That(schema.Properties.Count, Is.EqualTo(3));
+        }
+
+        private Schema Deserialize(string schema)
+        {
+            var settings = new JsonSerializerSettings()
+            {
+                NullValueHandling = NullValueHandling.Ignore,
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                DateFormatHandling = DateFormatHandling.IsoDateFormat
+            };
+
+            return JsonConvert.DeserializeObject<Schema>(schema, settings);
+        }
+    }
+}
